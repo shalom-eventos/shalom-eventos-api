349075be22acbf339332842e13a6bcbb90ed65db
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-05 13:50:22 -0300

    feat: create verify-jwt and verify-user-role middlewares

--- src/shared/infra/http/routes.ts
+++ src/shared/infra/http/routes.ts
@@ -3,9 +3,13 @@ import { FastifyInstance } from 'fastify';
 import { sessionsRoutes } from '@/modules/users/http/routes/sessions-routes';
 import { userRoutes } from '@/modules/users/http/routes/users-routes';
 import { profileRoutes } from '@/modules/users/http/routes/profile-routes';
+import { eventsRoutes } from '@/modules/events/http/routes/events-routes';
+import { tokenRoutes } from '@/modules/users/http/routes/token-routes';
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(userRoutes);
   app.register(sessionsRoutes);
   app.register(profileRoutes);
+  app.register(eventsRoutes);
+  app.register(tokenRoutes);
 }
