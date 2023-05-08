710c61af033f91801042ecc5a45e041d8cc66e12
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-08 19:24:16 -0300

    fix: fix makeCreateTicketUseCase

--- src/modules/event-tickets/use-cases/factories/make-create-ticket-use-case.ts
+++ src/modules/event-tickets/use-cases/factories/make-create-ticket-use-case.ts
@@ -1,10 +1,12 @@
+import { PrismaEventsRepository } from '@/modules/events/repositories/prisma/prisma-events-repository';
 import { PrismaTicketsRepository } from '../../repositories/prisma/prisma-addresses-repository';
 import { CreateTicketUseCase } from '../create-ticket-use-case';
 
 export function makeCreateTicketUseCase() {
   const ticketsRepository = new PrismaTicketsRepository();
+  const eventsRepository = new PrismaEventsRepository();
 
-  const useCase = new CreateTicketUseCase(ticketsRepository);
+  const useCase = new CreateTicketUseCase(ticketsRepository, eventsRepository);
 
   return useCase;
 }
