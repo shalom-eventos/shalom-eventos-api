6842d445d5c8a87ee9b5dce1f992891d9e1255e2
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-08 19:29:10 -0300

    feat: add payment to global routes

--- src/shared/infra/http/routes.ts
+++ src/shared/infra/http/routes.ts
@@ -8,6 +8,7 @@ import { tokenRoutes } from '@/modules/users/http/routes/token-routes';
 import { addressRoutes } from '@/modules/addresses/http/routes/address-routes';
 import { ticketsRoutes } from '@/modules/event-tickets/http/routes/tickets-routes';
 import { registrationsRoutes } from '@/modules/event-registrations/http/routes/registrations-routes';
+import { paymentsRoutes } from '@/modules/payments/http/routes/payments-routes';
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(userRoutes);
@@ -18,4 +19,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.register(addressRoutes);
   app.register(ticketsRoutes);
   app.register(registrationsRoutes);
+  app.register(paymentsRoutes);
 }
