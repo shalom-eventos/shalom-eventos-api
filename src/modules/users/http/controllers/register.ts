0c0554bc86ee6bfab49d8ce552afa34cad7d0066
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-14 04:36:07 -0300

    feat: use 8 as minimum password lenght

--- src/modules/users/http/controllers/register.ts
+++ src/modules/users/http/controllers/register.ts
@@ -9,8 +9,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     .object({
       name: z.string(),
       email: z.string().email(),
-      password: z.string().min(6),
-      password_confirmation: z.string().min(6),
+      password: z.string().min(8),
+      password_confirmation: z.string().min(8),
     })
     .strict()
     .refine((data) => data.password === data.password_confirmation, {
