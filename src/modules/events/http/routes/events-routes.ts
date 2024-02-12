4ec363182885012709b8470ed24e390f4b34345b
Willian S. Praciano <willian.s.praciano@gmail.com>
2024-02-12 14:47:48 -0300

    chore: move events controller directly to controller folder

--- src/modules/events/http/routes/events-routes.ts
+++ src/modules/events/http/routes/events-routes.ts
@@ -1,11 +1,11 @@
 import { FastifyInstance } from 'fastify';
 
-import { createEventController } from '../controllers/events/create-event-controller';
+import { createEventController } from '../controllers/create-event-controller';
 import { verifyUserRole } from '@/shared/infra/http/middlewares/verify-user-role';
 import { verifyJWT } from '@/shared/infra/http/middlewares/verify-jwt';
-import { getEventController } from '../controllers/events/get-event-controller';
-import { updateEventController } from '../controllers/events/update-event-controller';
-import { listEventsController } from '../controllers/events/list-events-controller';
+import { getEventController } from '../controllers/get-event-controller';
+import { updateEventController } from '../controllers/update-event-controller';
+import { listEventsController } from '../controllers/list-events-controller';
 
 export async function eventsRoutes(app: FastifyInstance) {
   app.get('/events/:id', getEventController);
