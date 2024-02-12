0607686f2b5ed84bde43c7af60fc3dea211ee283
Willian S. Praciano <willian.s.praciano@gmail.com>
2024-02-12 19:08:59 -0300

    feat: add all repositories to diContainer

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,12 +2,32 @@ import { asClass, Lifetime } from 'awilix';
 import { di } from '@/shared/lib/diContainer';
 import { PrismaUsersRepository } from '@/modules/users/repositories/prisma/prisma-users-repository';
 import { PrismaEventsRepository } from '@/modules/events/repositories/prisma/prisma-events-repository';
+import { PrismaAddressesRepository } from '@/modules/addresses/repositories/prisma/prisma-addresses-repository';
+import { PrismaRegistrationsRepository } from '@/modules/event-registrations/repositories/prisma/prisma-registrations-repository';
+import { PrismaTicketsRepository } from '@/modules/event-tickets/repositories/prisma/prisma-addresses-repository';
+import { PrismaParticipantsRepository } from '@/modules/participants/repositories/prisma/prisma-participants-repository';
+import { PrismaPaymentsRepository } from '@/modules/payments/repositories/prisma/prisma-payments-repository';
 
 di.register({
   usersRepository: asClass(PrismaUsersRepository, {
     lifetime: Lifetime.SINGLETON,
   }),
   eventsRepository: asClass(PrismaEventsRepository, {
+    lifetime: Lifetime.SINGLETON,
+  }),
+  addressesRepository: asClass(PrismaAddressesRepository, {
+    lifetime: Lifetime.SINGLETON,
+  }),
+  registrationsRepository: asClass(PrismaRegistrationsRepository, {
+    lifetime: Lifetime.SINGLETON,
+  }),
+  ticketsRepository: asClass(PrismaTicketsRepository, {
+    lifetime: Lifetime.SINGLETON,
+  }),
+  participantsRepository: asClass(PrismaParticipantsRepository, {
+    lifetime: Lifetime.SINGLETON,
+  }),
+  paymentsRepository: asClass(PrismaPaymentsRepository, {
     lifetime: Lifetime.SINGLETON,
   }),
 });
